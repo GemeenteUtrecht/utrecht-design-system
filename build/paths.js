9307e323236647d487c4c83e8bddae7b00e3a74b
Sergei Maertens <sergei@maykinmedia.nl>
2020-03-11 09:34:45 +0100

    :construction_worker: Add build system

---
+++ build/paths.js
@@ -0,0 +1,57 @@
+var path = require('path');
+var fs = require('fs');
+
+
+/** Parses package.json */
+var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+
+/** Name of the sources directory */
+var sourcesRoot = 'src/';
+
+/** Name of the static (source) directory */
+var distRoot = 'dist/';
+
+
+/**
+ * Application path configuration for use in frontend scripts
+ */
+module.exports = {
+    // Parsed package.json
+    package: pkg,
+
+    // Path to the sass (sources) directory
+    sassSrcDir: sourcesRoot + 'sass/',
+
+    // Path to the sass (sources) entry point
+    sassSrc: sourcesRoot + 'sass/**/*.scss',
+
+    // Path to the (transpiled) css directory
+    cssDir: distRoot + 'css/',
+
+    // Path to the fonts directory
+    fontsDir: distRoot + 'fonts/',
+
+    // Path to the js entry point (source)
+    jsEntry: sourcesRoot + 'js/index.js',
+
+    // Path to the js (sources) directory
+    jsSrcDir: sourcesRoot + 'js/',
+
+    // Path to js (sources)
+    jsSrc: sourcesRoot + 'js/**/*.js',
+
+    // Path to the js (sources) directory
+    jsSrcDir: sourcesRoot + 'js/',
+
+    // Path to the (transpiled) js directory
+    jsDir: distRoot,
+
+    // Path to js spec (test) files
+    jsSpec: sourcesRoot + 'jstests/**/*.spec.js',
+
+    // Path to js spec (test) entry file
+    jsSpecEntry: sourcesRoot + 'jstests/index.js',
+
+    // Path to js code coverage directory
+    coverageDir: 'reports/jstests/',
+};
